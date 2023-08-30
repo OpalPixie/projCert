aef4c899ce005523badbbbe788f062a4dc78dd9d
OpalPixie <saswatiaradhna@gmail.com>
2023-08-30 17:58:40 +0000

    rebase done

--- new-repo/pyfile.py
+++ new-repo/pyfile.py
@@ -2,11 +2,16 @@ a = 5
 b = 6
 c = 8
 d = 10
+<<<<<<< HEAD
 
 f = 20
 g = 25
 i = 30
 j = 50
 sum = a+b+c+d+f+g+i
+=======
+e = 12
+sum = a+b+c+d+e
+>>>>>>> 4935764 (third commit)
 print "sum of two numbers is " sum
 
